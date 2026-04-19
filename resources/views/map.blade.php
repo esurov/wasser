d36b2533e200de5fcb6e4c8f21c09572734bb901
Gene Surov <esurov@gmail.com>
2026-04-19 15:23:12 +0200

    rename page title to MyWasser

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Drinking Fountains Near You</title>
+    <title>MyWasser</title>
 
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
